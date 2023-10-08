5c2616db46a647bb01c5fdc2f39af5811cbddfe2
Anton Zhade <anton.zhade@simbirsoft@gmail.com>
2023-10-08 16:12:08 +0300

    some descriptive message here

---
+++ src/config/MUI/theme.ts
@@ -0,0 +1,15 @@
+import { ThemeOptions, createTheme } from '@mui/material/styles';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#9c27b0',
+    },
+  },
+};
+
+export const theme = createTheme(themeOptions);
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./config/MUI/theme.ts";
+import App from "./App.tsx";
+import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-)
+  </ThemeProvider>
+);
